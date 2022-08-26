d605a9d0edd80131418c616ccca596cdb10209e4
Gabriel Baskin-Ridley <gbaskinridley@gmail.com>
2022-08-26 14:26:10 +0000

    set up mongoDB

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 import express from "express"
+import dotenv from "dotenv"
+import mongoose from "mongoose"
 const app = express()
+dotenv.config()
+
+const connect = async () => {
+    try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("connection successful with mongoDB")
+    } catch (error) {
+    handleError(error);
+    }
+}
+
 
 app.listen(8800, () => {
+    connect()
     console.log("connected")
 })
 
