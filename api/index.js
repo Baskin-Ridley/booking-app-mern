0e8a816990f72ec47017a7b2fde809d21fb9a5bd
Gabriel Baskin-Ridley <gbaskinridley@gmail.com>
2022-08-27 08:43:14 +0000

    Updated auth and imports

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
 import express from "express"
 import dotenv from "dotenv"
 import mongoose from "mongoose"
+import authRoute from "./routes/auth.js"
+import usersRoute from "./routes/users.js"
+import hotelsRoute from "./routes/hotels.js"
+import roomsRoute from "./routes/rooms.js"
 const app = express()
 dotenv.config()
 
@@ -20,6 +24,12 @@ mongoose.connection.on("disconnected", ()=>{
 mongoose.connection.on("connected", ()=>{
     console.log("mongoDB connected")
 })
+
+//middleware
+app.use("/api/auth", authRoute);
+app.use("/api/users", authRoute);
+app.use("/api/hotels", authRoute);
+app.use("/api/rooms", authRoute);
 
 app.listen(8800, () => {
     connect()
