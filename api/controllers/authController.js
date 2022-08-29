81d2b3f9d2359d3f999aac92711c85eeb5be8646
Gabriel Baskin-Ridley <gbaskinridley@gmail.com>
2022-08-29 11:24:37 +0000

    installed and set up cookie-parser

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -4,6 +4,8 @@ import { createError } from "../utils/error.js"
 import jwt from "jsonwebtoken"
 import dotenv from "dotenv"
 
+
+
 export const register = async (req,res,next)=>{
     try {
         const salt = bcrypt.genSaltSync(10);
@@ -33,7 +35,11 @@ export const login = async (req,res,next)=>{
         const token = jwt.sign( {id: user._id, isAdmin: user.isAdmin }, process.env.JWT)
 
         const {password, isAdmin, ...otherDetails } = user._doc
-        res.status(200).json({...otherDetails})
+        res.cookie("access_token", token, {
+            httpOnly: true,
+        })
+        .status(200)
+        .json({...otherDetails})
     }catch(err){
         next(err)
     }
