a34163a996bc403a6e753afa148107393c6e7300
Gabriel Baskin-Ridley <gbaskinridley@gmail.com>
2022-08-29 11:19:04 +0000

    added jwt to auth

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,6 +1,9 @@
 import User from "../models/User.js"
 import bcrypt from "bcryptjs"
 import { createError } from "../utils/error.js"
+import jwt from "jsonwebtoken"
+import dotenv from "dotenv"
+
 export const register = async (req,res,next)=>{
     try {
         const salt = bcrypt.genSaltSync(10);
@@ -20,10 +23,14 @@ export const register = async (req,res,next)=>{
 export const login = async (req,res,next)=>{
     try {
         const user = await User.findOne({username:req.body.username})
-        if(!user) return next (createError(404, "User not found!"))
+        if(!user) 
+            return next (createError(404, "User not found!"))
 
         const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password)
-        if(!isPasswordCorrect) return next (createError(404, "Wrong password or username!"))
+        if(!isPasswordCorrect) 
+            return next (createError(404, "Wrong password or username!"))
+
+        const token = jwt.sign( {id: user._id, isAdmin: user.isAdmin }, process.env.JWT)
 
         const {password, isAdmin, ...otherDetails } = user._doc
         res.status(200).json({...otherDetails})
