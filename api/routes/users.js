d26e3c7e5928c499353e43739bb0d3a8107878d2
Gabriel Baskin-Ridley <gbaskinridley@gmail.com>
2022-08-29 15:00:53 +0000

    added commands to change users

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,6 +1,19 @@
 import express from "express"
-
+import { updateUser, deleteUser, getUser, getAllUsers } from "../controllers/userController.js"
 const router = express.Router();
+
+//update
+router.put("/:id", updateUser)
+
+//delete
+router.delete("/:id", deleteUser)
+//get
+router.get("/:id", getUser)
+
+//get all
+router.get("/", getAllUsers)
+
+
 
 
 export default router
