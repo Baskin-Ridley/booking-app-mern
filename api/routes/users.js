cc6db9e4db819e244650005bde5b6b045e0b6364
Gabriel Baskin-Ridley <gbaskinridley@gmail.com>
2022-08-30 06:02:55 +0000

    Added account verification

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,6 +1,15 @@
 import express from "express"
 import { updateUser, deleteUser, getUser, getAllUsers } from "../controllers/userController.js"
+import { verifyToken } from "../utils/verifyToken.js"
 const router = express.Router();
+
+router.get("/checkauthentication", verifyToken, (req,res,next)=>{
+    res.send("You are authenticated")
+})
+
+router.get("/checkuser/:id", verifyToken, (req,res,next)=>{
+    res.send("You are logged in")
+})
 
 //update
 router.put("/:id", updateUser)
