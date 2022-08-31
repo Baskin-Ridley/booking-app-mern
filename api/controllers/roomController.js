74a096f45ecf0636467ea2c60797f46714254a63
Gabriel Baskin-Ridley <gbaskinridley@gmail.com>
2022-08-31 15:25:51 +0000

    udated roomcontroller and room

--- api/controllers/roomController.js
+++ api/controllers/roomController.js
@@ -21,3 +21,62 @@ export const createRoom = async (req, res, next) => {
       }
     };
     
+    export const updateRoom = async (req,res,next)=>{
+      try{
+          const updatedRoom = await Room.findByIdAndUpdate(
+              req.params.id,
+              { $set: req.body },
+              {new: true }
+          );
+          res.status(200).json(updatedRoom);
+          //200 is request has succeeded
+      } catch (err) {
+          next(err)
+          //500 is server error
+      }
+  
+  }
+  
+  export const deleteRoom = async (req,res,next)=>{
+      try{
+          await Room.findByIdAndDelete(
+              req.params.id
+  
+          );
+          res.status(200).json("deleted Room");
+          //200 is request has succeeded
+      } catch (err) {
+          next(err)
+          //500 is server error
+      }
+      
+  }
+  
+  export const getRoom = async (req,res,next)=>{
+      try{
+          const room = await Room.findById(
+              req.params.id
+          );
+          res.status(200).json(room);
+          //200 is request has succeeded
+      } catch (err) {
+          next(err)
+          //500 is server error
+      }
+      
+  }
+  
+  
+  
+  export const getAllRooms = async (req,res,next)=>{
+      try{
+          const Rooms = await Room.find()
+          res.status(200).json(rooms);
+          //200 is request has succeeded
+      } catch (err) {
+          next(err)
+          //500 is server error
+      }
+      
+  }
+  
--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,6 +1,23 @@
 import express from "express"
 
 const router = express.Router();
+import { createRoom, getAllRooms, updateRoom, deleteRoom, getRoom } from "../controllers/roomController.js"
+import { verifyAdmin } from "../utils/verifyToken.js"
+
+//create
+router.post("/", verifyAdmin, createRoom)
+
+//update
+router.put("/:id", verifyAdmin, updateRoom)
+
+//delete
+router.delete("/:id", verifyAdmin, deleteRoom)
+//get
+router.get("/:id", getRoom)
+
+//get all
+router.get("/", getAllRooms)
+
 
 
 export default router
