b1b0cbbe330e66d99394cc4c5b60f2bc7373660e
Gabriel Baskin-Ridley <gbaskinridley@gmail.com>
2022-09-03 07:49:36 +0000

    removed uneeded lines

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,8 @@
 
 import React from 'react';
-import logo from './logo.svg';
+
 import './sass/style.css';
-import {BrowserRouter, Router, Route, Routes} from "react-router-dom"
+import {BrowserRouter, Route, Routes} from "react-router-dom"
 
 import Home from './pages/home/Home';
 import List from './pages/list/List';
