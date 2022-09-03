cecdda49769e7bb48081ae10ee017b354c68551c
Gabriel Baskin-Ridley <gbaskinridley@gmail.com>
2022-09-03 09:34:42 +0000

    issue was not with code but CORS in chrome

--- client/src/hooks/useFetch.tsx
+++ client/src/hooks/useFetch.tsx
@@ -13,9 +13,13 @@ const useFetch = (url: any) => {
         setLoading(true);
         try {
           const res = await axios.get(url);
+
           setData(res.data);
+          console.log("success fetching data")
+          console.log(data)
         } catch (err) {
           setError(err);
+          console.log("error fetching data")
         }
         setLoading(false);
       };
