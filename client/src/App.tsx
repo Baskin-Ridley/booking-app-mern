0b8c4709d0d672e72cc995d20aebda13a6225bdb
Gabriel Baskin-Ridley <gbaskinridley@gmail.com>
2022-09-03 07:51:43 +0000

    readded basename so build works locally

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,7 +9,7 @@ import List from './pages/list/List';
 import Hotel from './pages/hotel/Hotel';
 function App() {
   return (
-    <BrowserRouter >
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/hotels" element={<List />} />
