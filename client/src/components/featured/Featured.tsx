da3bcf37757f0a7d341d033e86d1fe3917feb04b
Gabriel Baskin-Ridley <gbaskinridley@gmail.com>
2022-09-03 09:23:54 +0000

    updated package.json with proxy

--- client/src/components/featured/Featured.tsx
+++ client/src/components/featured/Featured.tsx
@@ -1,38 +1,42 @@
 import React from 'react'
-import "./featured.css" 
+import "./featured.css"
+import useFetch from '../../hooks/useFetch'
 const Featured = () => {
-  return (
-    <div className="featured">
-        <div className="featuredItem">
-            <img src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" 
-            alt="placeholder"
-            className="featuredImg"/>
-            <div className="featuredTitles">
-                <h1>London</h1>
-                <h1>224 properties</h1>
-            </div>
-        </div>
-        <div className="featuredItem">
-            <img src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" 
-            alt="placeholder"
-            className="featuredImg"/>
-            <div className="featuredTitles">
-                <h1>London</h1>
-                <h1>242 properties</h1>
-            </div>
-        </div>
-        <div className="featuredItem">
-            <img src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" 
-            alt="placeholder"
-            className="featuredImg"/>
-            <div className="featuredTitles">
-                <h1>London</h1>
-                <h1>304 properties</h1>
-            </div>
-        </div>
 
-    </div>
-  )
+    const { data, loading, error } = useFetch("")
+
+    return (
+        <div className="featured">
+            <div className="featuredItem">
+                <img src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" 
+                alt="placeholder"
+                className="featuredImg"/>
+                <div className="featuredTitles">
+                    <h1>London</h1>
+                    <h1>224 properties</h1>
+                </div>
+            </div>
+            <div className="featuredItem">
+                <img src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" 
+                alt="placeholder"
+                className="featuredImg"/>
+                <div className="featuredTitles">
+                    <h1>London</h1>
+                    <h1>242 properties</h1>
+                </div>
+            </div>
+            <div className="featuredItem">
+                <img src="https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" 
+                alt="placeholder"
+                className="featuredImg"/>
+                <div className="featuredTitles">
+                    <h1>London</h1>
+                    <h1>304 properties</h1>
+                </div>
+            </div>
+
+        </div>
+    )
 }
 
 export default Featured 
