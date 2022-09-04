9d9150d9e0b6c1f5583893d33a962b5f715dfa33
Gabriel Baskin-Ridley <gbaskinridley@gmail.com>
2022-09-04 10:52:31 +0000

    added cors

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ import usersRoute from "./routes/users.js"
 import hotelsRoute from "./routes/hotels.js"
 import roomsRoute from "./routes/rooms.js"
 import cookieParser from "cookie-parser"
-
+import cors from "cors";
 const app = express()
 dotenv.config()
 
@@ -28,6 +28,8 @@ mongoose.connection.on("connected", ()=>{
 })
 
 //middleware
+
+app.use(cors())
 app.use(cookieParser())
 app.use(express.json())
 
