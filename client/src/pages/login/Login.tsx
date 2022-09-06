f72011f08e7a94358f2abeae60822325c0652c84
Gabriel Baskin-Ridley <gbaskinridley@gmail.com>
2022-09-06 10:54:30 +0000

    build login page

--- client/src/pages/login/Login.tsx
+++ client/src/pages/login/Login.tsx
@@ -1,8 +1,26 @@
+import { useContext, useState } from "react"
+import { AuthContext } from "../../context/AuthContext";
 import "./login.css"
 
 const Login = () => {
+  const [credentials, setCredentials] = useState({
+    username:undefined,
+    password:undefined
+  })
+
+  const { loading, error, dispatch } = useContext(AuthContext);
+
+  function handleChange() {
+    console.log("handlechange")
+  }
+
   return (
-    <div>Login</div>
+    <div className="login">
+      <div className="lContainer">
+        <input type="text" placeholder="username" id="username" onChange={handleChange} className="lInput" />
+        <input type="password" placeholder="password" id="username" onChange={handleChange} className="lInput" />
+      </div>
+    </div>
   )
 }
 
