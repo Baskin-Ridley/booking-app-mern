1bae28f2994a8eb5fdbc0c1bb08e8e9c9e1acfbc
Gabriel Baskin-Ridley <gbaskinridley@gmail.com>
2022-09-06 11:47:36 +0000

    fixed login 🥳

--- client/src/pages/login/Login.tsx
+++ client/src/pages/login/Login.tsx
@@ -23,14 +23,11 @@ const Login = () => {
     e.preventDefault();
     dispatch({ type: "LOGIN_START" });
     try {
-      {const res = await axios.post("https://8800-baskinridle-bookingappm-f2ixwev9f56.ws-eu63.gitpod.io/api/auth/login", credentials);}
+      const res = await axios.post("https://8800-baskinridle-bookingappm-f2ixwev9f56.ws-eu63.gitpod.io/api/auth/login", credentials);
 
       dispatch({ type: "LOGIN_SUCCESS", payload: res.data.details });
       navigate("/")
     } catch (err) {
-
-
-
       dispatch({ type: "LOGIN_FAILURE", payload: err.response.data });
     }
   };
