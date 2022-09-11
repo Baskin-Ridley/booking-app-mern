bd201235c89aa17e756c714f0de32e6d0543f1dc
Gabriel Baskin-Ridley <gbaskinridley@gmail.com>
2022-09-11 09:14:19 +0000

    mapped room to the page

--- client/src/components/reserve/Reserve.tsx
+++ client/src/components/reserve/Reserve.tsx
@@ -10,6 +10,15 @@ const Reserve = ({ setOpen, hotelId }) => {
       <div className="rContainer">
         <FontAwesomeIcon icon={faCircleXmark} className="rClose" onClick={() => setOpen(false)}/>
         <span>Select your rooms:</span>
+        {data.map(item => (
+          <div className="rItem">
+            <div className="rItemInfo">
+              <div className="rTitle">{item.title}</div>
+              <div className="rDesc">{item.desc}</div>
+              <div className="rMax">Max People: <b>{item.maxPeople}</b></div>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   )
