4f5a32e193194598731dd72dd6511070500453fd
Gabriel Baskin-Ridley <gbaskinridley@gmail.com>
2022-09-11 09:11:13 +0000

    added fetch url

--- client/src/components/reserve/Reserve.tsx
+++ client/src/components/reserve/Reserve.tsx
@@ -3,6 +3,8 @@ import { faCircleXmark } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import "./reserve.css"
 const Reserve = ({ setOpen, hotelId }) => {
+
+  const { data, loading, error } = useFetch(`https://8800-baskinridle-bookingappm-f2ixwev9f56.ws-eu64.gitpod.io/api/hotels/room/${hotelId}`)
   return (
     <div className="reserve">
       <div className="rContainer">
