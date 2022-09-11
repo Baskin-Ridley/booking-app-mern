2403105b0feb186e83b19fc8c951fe6d4f2bcf8e
Gabriel Baskin-Ridley <gbaskinridley@gmail.com>
2022-09-11 13:28:16 +0000

    added useState import

--- client/src/components/reserve/Reserve.tsx
+++ client/src/components/reserve/Reserve.tsx
@@ -2,7 +2,7 @@
 import { faCircleXmark } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import useFetch from "../../hooks/useFetch"
-
+import { useState } from "react"
 import "./reserve.css"
 const Reserve = ({ setOpen, hotelId }) => {
 
