3c4df5fe773c13a18b0ba9136e40e1241321aae4
Baskin-Ridley <gbaskinridley@gmail.com>
2022-09-14 16:10:04 +0100

    fixed css iamge sizes

--- client/src/components/searchItem/SearchItem.tsx
+++ client/src/components/searchItem/SearchItem.tsx
@@ -11,7 +11,7 @@ const SearchItem = ({item}: any) => {
       <div className="siDesc">
         <h1 className="siTitle">{item.name}</h1>
         <span className="siDistance">{item.distance}m from city centre</span>
-        <span className="siTaxiOp">{item.desc}</span>
+        <span className="siTaxiOp">{item.title}</span>
         <span className="siSubtitle">
           Studio Apartment with Air conditioning
         </span>
