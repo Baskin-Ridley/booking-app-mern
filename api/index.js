e44f34ce9a5212af64f244eb222c25eeb9a19d1b
Baskin-Ridley <gbaskinridley@gmail.com>
2022-09-16 18:30:09 +0100

    edited index

--- api/index.js
+++ api/index.js
@@ -67,11 +67,6 @@ app.listen(process.env.PORT || 8800, () => {
 
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve('../client/build')));
-
-// Handle GET requests to /api route
-app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
-  });
   
   // All other GET requests not handled before will return our React app
   app.get('*', (req, res) => {
