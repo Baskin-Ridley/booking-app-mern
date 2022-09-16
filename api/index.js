0287bc3983783e9b490364de97a5661b7a053d9f
Baskin-Ridley <gbaskinridley@gmail.com>
2022-09-16 20:42:39 +0100

    updated index symbol

--- api/index.js
+++ api/index.js
@@ -75,6 +75,6 @@ app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 
 // All other GET requests not handled before will return our React app
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 }); 
