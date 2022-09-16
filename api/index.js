d4ba16f628a5f8424ac2eb2a17def0f732ba1ee1
Baskin-Ridley <gbaskinridley@gmail.com>
2022-09-16 18:13:16 +0100

    updated routing

--- api/index.js
+++ api/index.js
@@ -67,3 +67,13 @@ app.listen(process.env.PORT || 8800, () => {
 
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve('../client/build')));
+
+// Handle GET requests to /api route
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello from server!" });
+  });
+  
+  // All other GET requests not handled before will return our React app
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  });
