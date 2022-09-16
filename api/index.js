14cac4ff71e69ae6528b568f90781739017fb57e
Baskin-Ridley <gbaskinridley@gmail.com>
2022-09-16 19:20:07 +0100

    changed path in index

--- api/index.js
+++ api/index.js
@@ -68,8 +68,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+//app.use(express.static(path.resolve(__dirname, '../client/build')));
 
+app.use(express.static(path.resolve('../client/build')));
 // Handle GET requests to /api route
 
 
