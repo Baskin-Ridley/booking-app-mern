e92395a721ef6de99baee520ecc32e3068e5b447
Baskin-Ridley <gbaskinridley@gmail.com>
2022-09-16 19:27:59 +0100

    changed to path.join

--- api/index.js
+++ api/index.js
@@ -75,6 +75,6 @@ app.use(express.static(path.resolve('../client/build')));
 
 
 // All other GET requests not handled before will return our React app
-/*app.use('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-}); */
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+}); 
